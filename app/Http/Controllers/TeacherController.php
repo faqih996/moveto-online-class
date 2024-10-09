9b581adef7579fe9fe85aa0d5bec50bb23c9eb30
faqih <faqih.syakir11@gmail.com>
2024-10-09 17:36:09 +0700

    add : teacher feature

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTeacherRequest;
 use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class TeacherController extends Controller
 {
@@ -12,7 +16,9 @@ class TeacherController extends Controller
      */
     public function index()
     {
-        //
+        $teachers = Teacher::orderByDesc('id')->get();
+
+        return view('admin.teachers.index', compact('teachers'));
     }
 
     /**
@@ -20,15 +26,46 @@ class TeacherController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.teachers.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTeacherRequest $request)
     {
         //
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if(!$user){
+            return back()->withErrors([
+                'email' => 'Data Tidak Ditemukan'
+            ]);
+        }
+
+        if ($user->hasRole('teacher')) {
+            return back()->withErrors([
+                'email' => 'Email tersebut sudah didaftarkan menjadi guru'
+            ]);
+        }
+
+        DB::transaction(function () use ($user, $validated) {
+
+            $validated['user_id'] = $user->id;
+            $validated['is_active'] = true;
+
+            Teacher::create($validated);
+
+            if($user->hasRole('student')){
+                $user->removeRole('student');
+            }
+
+            $user->assignRole('teacher');
+        });
+
+        return redirect()->route('admin.teachers.index')->with('success', 'Teacher created successfully');
     }
 
     /**
@@ -60,6 +97,21 @@ class TeacherController extends Controller
      */
     public function destroy(Teacher $teacher)
     {
-        //
+        try {
+
+            $teacher->delete();
+
+            $user = User::find($teacher->user_id);
+            $user->removeRole('teacher');
+            $user->assignRole('student');
+
+            return redirect()->back();
+        } catch (\Exception $e) {
+            DB::rollback();
+            $error = ValidationException::withMessages([
+                'system_error' => ['System Error!', $e->getMessage()],
+            ]);
+            throw $error;
+        }
     }
 }
